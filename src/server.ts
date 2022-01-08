4b7a4327e5f72f0ca4b07fb4255aa21e6d1ea4bd
Luiz Jesuz <luizjesuz.p@gmail.com>
2022-01-07 21:36:01 -0300

    chore: add server config files

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,51 @@
-import { Application } from 'express';
-import { Server } from '@overnightjs/core';
-import bodyParser from 'body-parser';
-import { DivisoresController } from './controllers/divisores.controller';
-
-// eslint-disable-next-line import/prefer-default-export
+import { Application } from 'express'
+import { Server } from '@overnightjs/core'
+import bodyParser from 'body-parser'
+import { DividersController } from './controllers/dividers.controller'
+import { DividersService } from './services/dividers.service'
+import * as http from 'http'
+import logger from './logger'
 export class SetupServer extends Server {
-  constructor(private port = 3000) {
-    super();
+  private server?: http.Server;
+  constructor (private port = 3000) {
+    super()
   }
 
-  public init(): void {
-    this.setupExpress();
-    this.setupControllers();
+  public async init (): Promise<void> {
+    this.setupExpress()
+    this.setupControllers()
   }
 
-  public getApp(): Application {
-    return this.app;
+  public getApp (): Application {
+    return this.app
   }
 
-  private setupExpress(): void {
-    this.app.use(bodyParser.json());
+  private setupExpress (): void {
+    this.app.use(bodyParser.json())
+    this.setupControllers()
   }
 
-  private setupControllers(): void {
-    const divisoresController = new DivisoresController();
-    this.addControllers([divisoresController]);
+  private setupControllers (): void {
+    const divisoresController = new DividersController(new DividersService())
+    this.addControllers([divisoresController])
+  }
+
+  public start (): void {
+    this.server = this.app.listen(this.port, () => {
+      logger.info('Server listening on port: ' + this.port)
+    })
+  }
+
+  public async close (): Promise<void> {
+    if (this.server) {
+      await new Promise((resolve, reject) => {
+        this.server?.close((err) => {
+          if (err) {
+            return reject(err)
+          }
+          resolve(true)
+        })
+      })
+    }
   }
 }
