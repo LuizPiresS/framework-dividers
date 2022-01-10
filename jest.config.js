6813ebba10295329ff19678de7a81ebed59e2972
Luiz Jesuz <luizjesuz.p@gmail.com>
2022-01-10 00:09:52 -0300

    refactory: edit test scripts

--- jest.config.js
+++ jest.config.js
@@ -2,13 +2,13 @@ module.exports = {
   roots: ['<rootDir>/tests'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
-    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/main/**'
   ],
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
   testEnvironment: 'node',
   transform: {
-    '.+\\.ts$': 'ts-jest',
-  },
+    '.+\\.ts$': 'ts-jest'
+  }
 
-};
+}
